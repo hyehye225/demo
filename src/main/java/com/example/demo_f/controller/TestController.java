59d4ba27954e85038407decf61b9624a2f1c0a1b
hyehye225 <55949875+hyehye225@users.noreply.github.com>
2023-04-05 20:57:50 +0900

    13. @ResponseBody 사용하기

--- src/main/java/com/example/demo_f/controller/TestController.java
+++ src/main/java/com/example/demo_f/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.example.demo_f.controller;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TestRequestBodyDTO;
 @RestController
 @RequestMapping("test")
@@ -16,6 +20,14 @@ public class TestController {
 	@GetMapping("/testRequestBody")
 	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
 		return "Hello World! ID"+testRequestBodyDTO.getId()+"Message :" +testRequestBodyDTO.getMessage();
+	}
+	@GetMapping("/testResponseBody") 
+	public ResponseDTO<String>testControllerResponseBody() {
+		List<String> list =new ArrayList<String>();
+		list.add("Hello World! I'm ResponseDTO");
+		list.add("See you!");
+		ResponseDTO<String> response=ResponseDTO.<String>builder().data(list).build();
+		return response;
 	}
 //	@GetMapping("/{id}")
 //	public String testControlllerWithPathVariables(@PathVariable(required=false) int id) {
