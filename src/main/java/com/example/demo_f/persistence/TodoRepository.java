b7c33dddf3aacca37f8b6227f82ad370672e020c
hyehye225 <55949875+hyehye225@users.noreply.github.com>
2023-04-05 21:39:26 +0900

    18. @NamedQuery, @Query 사용하기

--- src/main/java/com/example/demo_f/persistence/TodoRepository.java
+++ src/main/java/com/example/demo_f/persistence/TodoRepository.java
@@ -3,11 +3,14 @@ package com.example.demo_f.persistence;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo_f.model.TodoEntity;
 
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity,String> {
-List<TodoEntity> findByUserId(String userId);
+//List<TodoEntity> searchByUserId(String userId);
+	@Query("select t from TodoEntity t where t.userId=?1")
+	List<TodoEntity> searchByUserId(String userId);
 }
