9f26e57ed680587541a532430ab25a8e99795813
hyehye225 <55949875+hyehye225@users.noreply.github.com>
2023-04-05 20:50:25 +0900

    12. @RequestBody 사용하기

--- src/main/java/com/example/demo_f/controller/TestController.java
+++ src/main/java/com/example/demo_f/controller/TestController.java
@@ -1,11 +1,10 @@
 package com.example.demo_f.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.example.demo.dto.TestRequestBodyDTO;
 @RestController
 @RequestMapping("test")
 public class TestController {
@@ -14,14 +13,17 @@ public class TestController {
 	public String testController() {
 		return "Hello World!(안녕하세요!)";
 	}
-	
-	@GetMapping("/{id}")
-	public String testControlllerWithPathVariables(@PathVariable(required=false) int id) {
-		return "Hello World! ID "+ id;
+	@GetMapping("/testRequestBody")
+	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+		return "Hello World! ID"+testRequestBodyDTO.getId()+"Message :" +testRequestBodyDTO.getMessage();
 	}
-	
-	@GetMapping("/Param")
-	public String testControllerRequestParam(@RequestParam(required=false) int id) {
-		return "Hello World! ID param " +id;
-	}
+//	@GetMapping("/{id}")
+//	public String testControlllerWithPathVariables(@PathVariable(required=false) int id) {
+//		return "Hello World! ID "+ id;
+//	}
+//	
+//	@GetMapping("/Param")
+//	public String testControllerRequestParam(@RequestParam(required=false) int id) {
+//		return "Hello World! ID param " +id;
+//	}
 }
